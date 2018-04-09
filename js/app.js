9c8ec0a479d824c46249413d92684d2fab6b1192
timsunpang <timsunpang@gmail.com>
2018-04-09 13:44:37 -0700

    finish voice and visuals pgs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
+	// NavLink and NavSection active links
 	$(".nav-link").click((e) => {
 		$(".nav-link").removeClass('active');
 		$(e.currentTarget).addClass('active');
+	})
+
+	$(".nav-section").click((e) => {
+		$(".nav-section").removeClass('active');
+		$(e.currentTarget).addClass('active');
+	})
+
+	// ClipboardJS plugin settings to copy hexcodes
+	const clipboard = new ClipboardJS(".copy-hex");
+
+
+	// Tooltip hide/show functions
+	const setTooltip = (btn, message) => {
+		$(btn).tooltip('hide')
+			.attr('data-original-title', message)
+			.tooltip('show');
+	}
+
+	const hideTooltip = (btn) => {
+		setTimeout(() => {
+			$(btn).tooltip('hide');
+		}, 2000);
+	}
+
+	clipboard.on('success', (e) => {
+		setTooltip(e.trigger, 'Copied!');
+		hideTooltip(e.trigger);
+	})
+
+	clipboard.on('error', (e) => {
+		setTooltip(e.trigger, 'Failed!');
+		hideTooltip(e.trigger);
 	})
 })
